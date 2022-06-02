93b8885b0e0e42cd49bb107f33d01584d1377355
srodif <srodweb@gmail.com>
2022-06-02 10:19:54 +0300

    served css through express static

--- myApp.js
+++ myApp.js
@@ -2,8 +2,10 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
+app.use('/public',express.static(__dirname + '/public'));
+
 app.get('/',function(req,res) {
-    res.sendFile(__dirname + "/views/index/html");
+    res.sendFile(__dirname + '/views/index/html');
 })
 
 
